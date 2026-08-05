d76cb52c11c50fe1e7a702e9b4b3871b2eec6e2b
reelmakerover <reelmakerover@gmail.com>
2026-08-05 16:22:26 +0530

    feat: increase express payload limits to support base64 images

--- server/index.js
+++ server/index.js
@@ -9,8 +9,9 @@ const app = express();
 
 // Middleware
 app.use(cors({ origin: true, credentials: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
